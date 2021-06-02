f33053f78298a055093d9d1d8d24bb6cee1d3557
Sadeq Brewen <brewen.sadeq@gmail.com>
2021-06-03 00:05:08 +0200

    Scene gym + proto (pas cape?)

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -27,6 +27,11 @@ public class GameplayManager : MonoBehaviour
         
         if(Input.GetKeyDown(KeyCode.R))
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+
+       
     }
 
     public void Death()
