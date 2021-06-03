98e0bf18bf394e90198c43f40cd908da2a4215bf
Barbas_Oyun <yoan.lechevalier@gmail.com>
2021-06-03 16:26:08 +0200

    Gamepad/keyboard detection

--- Assets/Scripts/Gears/SavePanel.cs
+++ Assets/Scripts/Gears/SavePanel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
 
 public class SavePanel : MonoBehaviour
 {
@@ -14,6 +15,15 @@ public class SavePanel : MonoBehaviour
     private Vector2 startPos;
     public Vector2 maxPos;
     private float progress;
+
+    public GameObject browseButton;
+    
+    PlayerInput _controls;
+    public static ControlDeviceType currentControlDevice;
+    public enum ControlDeviceType{
+        KeyboardAndMouse,
+        Gamepad,
+    }
     
     void Start()
     {
@@ -21,6 +31,18 @@ public class SavePanel : MonoBehaviour
 
         startPos = scrollable.position;
         maxPos = startPos + maxPos;
+
+        if (Gamepad.current != null)
+        {
+            browseButton.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("No GamePad");
+        }
+        
+        //_controls = Gears.gears.playerInput;
+        //_controls.onControlsChanged += OnControlsChanged;
     }
     
     void Update()
@@ -32,9 +54,59 @@ public class SavePanel : MonoBehaviour
         scrollable.position = Vector2.Lerp(startPos, maxPos, progress);
     }
 
+    void OnEnable() {
+        InputUser.onChange += onInputDeviceChange;
+    }
+ 
+    void OnDisable() {
+        InputUser.onChange -= onInputDeviceChange;
+    }
+
     public void UpdatePathText(string s)
     {
         path = s;
         pathText.text = $"path : {s}";
+    }
+    
+    //Solution 1
+    void onInputDeviceChange(InputUser user, InputUserChange change, InputDevice device)
+    {
+        if (change == InputUserChange.ControlSchemeChanged && user.controlScheme != null) 
+        {
+            switch (user.controlScheme.Value.name)
+            {
+                case "Gamepad" :
+                    Debug.Log("last device used : Gamepad");
+                    break;
+                case "Keyboard" :
+                    Debug.Log("last device used : Keyboard");
+                    break;
+            }
+        }
+    }
+    
+    //Solution 2
+    private void OnControlsChanged(PlayerInput obj)
+    {
+        if (obj.currentControlScheme == "Gamepad")
+        {
+            if (currentControlDevice != ControlDeviceType.Gamepad)
+            {
+                currentControlDevice = ControlDeviceType.Gamepad;
+                // Send Event
+                // EventHandler.ExecuteEvent("DeviceChanged", currentControlDevice);
+            }
+        }
+        else
+        {
+            if (currentControlDevice != ControlDeviceType.KeyboardAndMouse)
+            {
+                currentControlDevice = ControlDeviceType.KeyboardAndMouse;
+                // Send Event
+                // EventHandler.ExecuteEvent("DeviceChanged", currentControlDevice);
+            }
+        }
+        
+        Debug.Log(currentControlDevice);
     }
 }
