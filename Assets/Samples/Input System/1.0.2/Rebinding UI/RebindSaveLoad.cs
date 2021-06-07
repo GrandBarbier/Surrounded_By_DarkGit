defd3f152894df48a538e0f7c60baf1ce872fedb
Barbas_Oyun <yoan.lechevalier@gmail.com>
2021-06-07 10:10:26 +0200

    Save Inputs

--- Assets/Samples/Input System/1.0.2/Rebinding UI/RebindSaveLoad.cs
+++ Assets/Samples/Input System/1.0.2/Rebinding UI/RebindSaveLoad.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Samples.RebindUI;
 
 public class RebindSaveLoad : MonoBehaviour
 {
    public InputActionAsset actions;
+   
+   public RebindActionUI[] RebindActionUis;
 
-   void OnEnable()
+   /*void OnEnable()
    {
       var rebinds = PlayerPrefs.GetString("rebinds");
       if (!string.IsNullOrEmpty(rebinds))
@@ -19,5 +23,32 @@ public class RebindSaveLoad : MonoBehaviour
    void OnDisable()
    {
       //var rebinds = actions.Save
+   }*/
+
+   void Awake()
+   {
+      InputAction[] actionsArrray = actions.ToArray();
+      
+      for (int i = 0; i < actions.Count(); i++)
+      {
+         if (!string.IsNullOrEmpty(PlayerPrefs.GetString(actionsArrray[i].name)))
+         {
+            //get saved binding index
+            int bindingIndex = PlayerPrefs.GetInt(actionsArrray[i].name + "bindingIndex");
+            
+            //create new input with saved input
+            InputBinding t = actionsArrray[i].bindings[bindingIndex];
+            t.overridePath = PlayerPrefs.GetString(actionsArrray[i].name + " key");
+            
+            //apply new input
+            actionsArrray[i].ChangeBinding(bindingIndex).To(t);
+            Debug.Log($"set saved input : {actionsArrray[i].name} -> {actionsArrray[i].bindings[bindingIndex].effectivePath}");
+         }
+      }
+
+      for (int i = 0; i < RebindActionUis.Count(); i++)
+      {
+         RebindActionUis[i].UpdateBindingDisplay();
+      }
    }
 }
