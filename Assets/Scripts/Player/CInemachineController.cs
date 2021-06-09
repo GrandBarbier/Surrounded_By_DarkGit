e554a03790f9334197f2c41f5b8906420781d120
Barbas_Oyun <yoan.lechevalier@gmail.com>
2021-06-09 11:16:54 +0200

    script rename

--- Assets/Scripts/Player/CInemachineController.cs
+++ Assets/Scripts/Player/CInemachineController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
 
-public class CInemachineController : MonoBehaviour
+public class CinemachineController : MonoBehaviour
 {
     public float lookSpeedX;
     public float lookSpeedY;
