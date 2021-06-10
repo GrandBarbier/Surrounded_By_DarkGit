b229b140194fa6d38dc496121b644b30c1ce858a
Gryfs <Allan18040@gmail.com>
2021-06-10 11:29:39 +0200

    Pressure Plate

---
+++ Assets/Scripts/Puzzles/pressingPlate.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Apple;
+
+public class pressingPlate : MonoBehaviour
+{
+    public GameObject door;
+    public Transform direction;
+    public GameObject link;
+
+    public bool isOpened = false;
+
+    void Update()
+    {
+        if (isOpened && link.GetComponent<pressingPlate>().isOpened == true)
+        {
+            Debug.Log("aled");
+
+            door.transform.position = Vector3.MoveTowards(door.transform.position, direction.position, Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    { 
+        isOpened = true;
+        
+        
+    }
+}
