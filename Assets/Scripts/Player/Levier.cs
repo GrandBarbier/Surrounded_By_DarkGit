f3825d21f9d60a68d3e2fdb76c68e3e5f40a432f
Gryfs <Allan18040@gmail.com>
2021-06-17 16:07:53 +0200

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Player/Levier.cs
+++ Assets/Scripts/Player/Levier.cs
@@ -8,37 +8,64 @@ public class Levier : MonoBehaviour
     public GameObject player;
     public Animator playerAnimator;
     public Transform direction;
-    public GameObject manche;
+    public Transform manche;
+    public Transform place;
+
+    public bool hanged;
 
     private void Start()
     {
         player = this.gameObject;
     }
+    
+    
 
     // Update is called once per frame
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("levier"))
         {
-            direction = other.gameObject.transform.GetChild(0);
+            direction = other.gameObject.transform.GetChild(2);
+            manche = other.gameObject.transform.GetChild(1);
+            place = manche.gameObject.transform.GetChild(0);
             if (Input.GetKey(KeyCode.A) && player.GetComponent<Movement>().isGrounded &&
                 player.GetComponent<PlaceTorch>().torchOnGround)
             {
-                Debug.Log("launch anim");
-                playerAnimator.SetBool("IsHanging", true);
-                other.transform.position = Vector3.MoveTowards(other.transform.position, direction.position, Time.deltaTime);
-                Debug.Log("launched anim");
                 
+                playerAnimator.SetBool("IsHanging", true);
+                player.GetComponent<Movement>().animPlaying = true;
+
             }
-            else
+
+            if (hanged)
+            {
+                player.transform.position = Vector3.MoveTowards(player.transform.position, place.position, Time.deltaTime * 4);
+                player.transform.rotation = place.transform.rotation;
+            }
+
+            if (player.transform.position == place.position)
+            {
+                manche.transform.position = Vector3.MoveTowards(manche.transform.position, direction.position, Time.deltaTime);
+            }
+            
+            
+            if(manche.position == direction.position)
             {
                 playerAnimator.SetBool("IsHanging", false);
+                Debug.Log("good");
+                player.GetComponent<Movement>().animPlaying = false;
             }
         } 
+    }
+
+    public void JumpHang()
+    {
+        hanged = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
         direction = null;
+        manche = null;
     }
 }
