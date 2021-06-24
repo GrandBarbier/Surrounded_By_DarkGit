0e2571e43e9eef73cbf0d425390c471dac87cfc5
Sadeq Brewen <brewen.sadeq@gmail.com>
2021-06-24 10:14:29 +0200

    Transition bgm OK (à installer mtn)

---
+++ Assets/SBDFMODbank/SetParameterByName.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetParameterByName : MonoBehaviour
+{
+    private FMOD.Studio.EventInstance instance;
+
+    [FMODUnity.EventRef]
+    public string fmodEvent;
+
+  
+    [Range(0f, 1f)]
+    public float introWater;
+    
+    [Range(0f, 1f)]
+    public float waterMaze;
+    
+    [Range(0f, 1f)]
+    public float mazeWind;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = FMODUnity.RuntimeManager.CreateInstance(fmodEvent);
+        instance.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        instance.setParameterByName("introWater", introWater);
+        instance.setParameterByName("waterMaze", waterMaze);
+        instance.setParameterByName("mazeWind", mazeWind);
+    }
+}
