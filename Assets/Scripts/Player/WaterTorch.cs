85319fa2f713a2c6bf488a39b4280a2f95430a5a
Lelio <lelio.rollin@e-artsup.net>
2021-06-28 11:40:34 +0200

    Setup save torch

--- Assets/Scripts/Player/WaterTorch.cs
+++ Assets/Scripts/Player/WaterTorch.cs
@@ -8,34 +8,24 @@ public class WaterTorch : MonoBehaviour
     public bool canBePicked;
     public GameObject t_light;
     public GameObject particles;
-    public AudioClip torch_extinction;
     public new AudioSource audio;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Test"))
         {
-            Debug.Log("PUTE");
             canBePicked = true;
         }
         else if (other.gameObject.CompareTag("water"))
         {
-            if (t_light.activeSelf)
-            {
-                audio.Stop();
-                audio.clip = torch_extinction;
-                audio.Play();
-            }
             t_light.SetActive(false);
             particles.SetActive(false);
         }
-        
     }
     
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Test"))
         {
-            Debug.Log("PAS PUTE");
             canBePicked = false;
         }
     }
